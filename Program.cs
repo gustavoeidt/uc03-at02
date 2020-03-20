949533926243b3ad6b76e202262e681aeb10560f
unknown <gustavo.eidt@gmail.com>
2020-03-20 09:47:27 -0300

    Inicial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace at02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaração das variáveis
+            String nome;
+            String senha;
+            int idade;
+            DateTime nascimento;
+            DateTime hoje;
+            CultureInfo cultura;
+
+            //Readline -> comando de leitura do nome
+            Console.WriteLine("Digite o seu nome");
+            nome = Console.ReadLine();
+
+            // Leitura do nome e criação do objeto DateTime
+            Console.WriteLine("Digite a sua data de nascimento(dd/mm/aaaa)");
+            cultura = new CultureInfo("pt-BR");
+            nascimento = Convert.ToDateTime(Console.ReadLine(), cultura);
+
+            // Cálculo da idade
+            hoje = DateTime.Today;
+            idade = hoje.Year - nascimento.Year;
+            if (hoje.Month < nascimento.Month || (hoje.Month == nascimento.Month && hoje.Day < nascimento.Day)){
+                idade--;
+            }
+
+            // Condição se tiver 18 ou mais
+            if(idade >= 18){
+                senha = nome+idade;
+            } else {
+                senha = idade+nome;
+            }
+
+            //Writeline -> Comando de escrita
+            Console.WriteLine("Sua senha forte é" + senha);
+
+        }
+    }
+}
